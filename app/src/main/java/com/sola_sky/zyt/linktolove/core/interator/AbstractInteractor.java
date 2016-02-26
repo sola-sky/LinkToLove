d6a9205ee601c90de9898d0641eada9cf2fb8e9e
Li Minyi <602699671@qq.com>
2016-02-26 23:51:31 +0800

    修改拼写错误

--- app/src/main/java/com/sola_sky/zyt/linktolove/core/interator/AbstractInteractor.java
+++ app/src/main/java/com/sola_sky/zyt/linktolove/core/interator/AbstractInteractor.java
@@ -2,36 +2,25 @@ package com.sola_sky.zyt.linktolove.core.interator;
 
 
 
-import com.sola_sky.zyt.linktolove.core.executor.BackgroundRun;
-
-import java.util.Collection;
-import java.util.List;
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Executor;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
+import com.sola_sky.zyt.linktolove.core.executor.BaseExecutor;
 
 /**
  * Created by Li on 2016/2/21.
  */
 public abstract class AbstractInteractor implements Interactor {
-    private BackgroundRun mExecutor;
+    private BaseExecutor mExecutor;
     public abstract void run();
 
 
     protected volatile boolean mIsCancel;
     protected volatile boolean mIsRunning;
 
-    public AbstractInteractor(BackgroundRun executor) {
+    public AbstractInteractor(BaseExecutor executor) {
         mExecutor = executor;
     }
     @Override
     public void execute() {
         this.mIsRunning = true;
-        mExecutor.exectue(this);
+        mExecutor.execute(this);
     }
 }
