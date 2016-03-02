b5784c74e517958b553361aef941b4049222d71e
Li Minyi <602699671@qq.com>
2016-03-03 01:13:01 +0800

    主要是增加了属性的引入和控件的测量

--- app/src/main/java/com/sola_sky/zyt/linktolove/view/CircleProgressBar.java
+++ app/src/main/java/com/sola_sky/zyt/linktolove/view/CircleProgressBar.java
@@ -4,9 +4,11 @@ import android.animation.Animator;
 import android.animation.ObjectAnimator;
 import android.animation.ValueAnimator;
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.drawable.Drawable;
 import android.os.Looper;
 import android.os.Message;
 import android.util.AttributeSet;
@@ -16,18 +18,30 @@ import android.view.animation.LinearInterpolator;
 
 import android.os.Handler;
 
+import com.sola_sky.zyt.linktolove.R;
+import com.sola_sky.zyt.linktolove.utils.DimensionUtils;
 
 
 /**
  * Created by Li on 2016/2/27.
  */
 public class CircleProgressBar extends View {
+
+    private static final float DEFAULT_RADIUS_DP = 40;
+    private static final String DEFAULT_START_COLOR = "#00ff00";
+    private static final String DEFAULT_END_COLOR = "#f76d09";
+    private static final float DEFAULT_INTERVAL = 2;
+
     private Paint mPaint;
-    private int mMaxRadius;
+
+
     private int mMinRadius;
+    private int mMaxRadius;
 
-    private int mDistance = 200;
-
+    private float mDistance = 200;
+    private String mStartColor;
+    private String mEndColor;
+    private Drawable mBgDrawable;
     private int drawColor = 0xff0000;
     private int mWidth;
     private int mHeight;
@@ -39,9 +53,12 @@ public class CircleProgressBar extends View {
     private float mIncrease;
     private boolean isFirstDraw = true;
     private Handler mHandler;
+    private int mType;
+
+    private Context mContext;
 
 
-    private ColorEvalutor mEvalutor;
+    private ColorEvaluator mEvaluator;
     private String color;
 
     public String getColor() {
@@ -56,22 +73,45 @@ public class CircleProgressBar extends View {
 
     public CircleProgressBar(Context context) {
         super(context);
-        init();
+        mContext = context;
+        init(null);
     }
 
     public CircleProgressBar(Context context, AttributeSet attrs) {
         super(context, attrs);
-        init();
+        mContext = context;
+        init(attrs);
     }
 
     public CircleProgressBar(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        init();
+        mContext = context;
+        init(attrs);
     }
 
 
-    private void init() {
-        mEvalutor = new ColorEvalutor();
+    private void init(AttributeSet attrs) {
+        float radius = DimensionUtils.dp2px(getContext(), DEFAULT_RADIUS_DP);
+
+        TypedArray ta = attrs == null ? null : getContext()
+                .obtainStyledAttributes(attrs, R.styleable.CircleProgressBar);
+        if (ta != null) {
+            mDistance = ta.getDimension(R.styleable.CircleProgressBar_radius, radius);
+            mStartColor = ta.getString(R.styleable.CircleProgressBar_start_color);
+            mEndColor = ta.getString(R.styleable.CircleProgressBar_end_color);
+            mType = ta.getInt(R.styleable.CircleProgressBar_type, 0);
+            mBgDrawable = ta.getDrawable(R.styleable.CircleProgressBar_background);
+            ta.recycle();
+        }
+
+        if (mStartColor == null) {
+            mStartColor = DEFAULT_START_COLOR;
+        }
+        if (mEndColor == null) {
+            mEndColor = DEFAULT_END_COLOR;
+        }
+
+        mEvaluator = new ColorEvaluator();
         mPaint = new Paint(Paint.ANTI_ALIAS_FLAG | Paint.DITHER_FLAG);
         mPaint.setStyle(Paint.Style.FILL);
         mPaint.setColor(Color.GREEN);
@@ -94,8 +134,8 @@ public class CircleProgressBar extends View {
     }
 
     private void startColorAnimation() {
-        mColorAnimator = ObjectAnimator.ofObject(this, "color", mEvalutor,
-                "#ffffff", "#00ff00");
+        mColorAnimator = ObjectAnimator.ofObject(this, "color", mEvaluator, mStartColor,
+                mEndColor);
         mColorAnimator.setDuration(5000);
         mColorAnimator.setRepeatMode(ObjectAnimator.REVERSE);
         mColorAnimator.setRepeatCount(ObjectAnimator.INFINITE);
@@ -118,9 +158,9 @@ public class CircleProgressBar extends View {
 
             @Override
             public void onAnimationRepeat(Animator animation) {
-                mEvalutor.setmCurrentBlue(-1);
-                mEvalutor.setmCurrentGreen(-1);
-                mEvalutor.setmCurrentRed(-1);
+                mEvaluator.setmCurrentBlue(-1);
+                mEvaluator.setmCurrentGreen(-1);
+                mEvaluator.setmCurrentRed(-1);
             }
         });
         mColorAnimator.start();
@@ -143,8 +183,25 @@ public class CircleProgressBar extends View {
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        int widthSize = MeasureSpec.getSize(widthMeasureSpec);
+        int widthMode = MeasureSpec.getMode(widthMeasureSpec);
+        int measureWidth;
+        int minWidth = (int) (mDistance * 2 + mMaxRadius * 2 + getPaddingLeft()
+                + getPaddingRight());
+        minWidth = Math.max(minWidth, getSuggestedMinimumWidth());
+        if (widthMode == MeasureSpec.EXACTLY) {
+            measureWidth = Math.max(minWidth, widthSize);
+        } else {
+            measureWidth = minWidth;
+            if (widthMode == MeasureSpec.AT_MOST) {
+                measureWidth = Math.min(measureWidth, widthSize);
+            }
+        }
+
+        setMeasuredDimension(measureWidth, measureWidth);
+
     }
+
 
     @Override
     protected void onSizeChanged(int w, int h, int oldw, int oldh) {
@@ -252,5 +309,9 @@ public class CircleProgressBar extends View {
         drawCircle(canvas, mDistance, mDegree, 65);
         mDegree = temp;
     //    postInvalidateDelayed(100);
+    }
+
+    enum Type {
+        Circle, Arc
     }
 }
