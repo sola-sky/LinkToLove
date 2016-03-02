2976e7a5120eb26a3bfddaf66a8907c5b56ba56f
Li Minyi <602699671@qq.com>
2016-03-03 01:11:36 +0800

    添加了尺寸单位转换的工具类

--- app/src/main/java/com/sola_sky/zyt/linktolove/utils/DimensionUtils.java
+++ app/src/main/java/com/sola_sky/zyt/linktolove/utils/DimensionUtils.java
@@ -1,7 +1,33 @@
 package com.sola_sky.zyt.linktolove.utils;
 
+import android.content.Context;
+import android.util.TypedValue;
+
 /**
  * Created by Li on 2016/3/2.
  */
 public class DimensionUtils {
+
+    private DimensionUtils() {
+
+    }
+    public static float dp2px(Context context, float dp) {
+        return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp,context.getResources()
+        .getDisplayMetrics());
+    }
+
+    public static float sp2px(Context context, float sp) {
+        return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, sp, context.getResources()
+        .getDisplayMetrics());
+    }
+
+    public static float px2dp(Context context, float px) {
+        float density = context.getResources().getDisplayMetrics().density;
+        return px / density;
+    }
+
+    public static float px2sp(Context context, float px) {
+        float scaledDensity = context.getResources().getDisplayMetrics().scaledDensity;
+        return px / scaledDensity;
+    }
 }
