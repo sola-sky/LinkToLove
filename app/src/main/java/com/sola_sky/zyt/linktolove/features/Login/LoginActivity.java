26538bfbb9ce627199f70d8c2a12c87806ce142b
Li Minyi <602699671@qq.com>
2016-03-06 14:31:32 +0800

    删除一些不要的代码和import

--- app/src/main/java/com/sola_sky/zyt/linktolove/features/Login/LoginActivity.java
+++ app/src/main/java/com/sola_sky/zyt/linktolove/features/Login/LoginActivity.java
@@ -1,20 +1,16 @@
 package com.sola_sky.zyt.linktolove.features.Login;
 
-import android.animation.ValueAnimator;
-import android.content.Intent;
 import android.support.v7.widget.Toolbar;
 import android.text.Editable;
 import android.text.TextWatcher;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
-import android.widget.Toast;
 
 import com.sola_sky.zyt.linktolove.R;
 import com.sola_sky.zyt.linktolove.app.BaseActivity;
-import com.sola_sky.zyt.linktolove.features.home.MainActivity;
+import com.sola_sky.zyt.linktolove.utils.IntentUtils;
 
 public class LoginActivity extends BaseActivity implements LoginPresenter.View{
 
@@ -28,7 +24,7 @@ public class LoginActivity extends BaseActivity implements LoginPresenter.View{
 
     private NotificationEnable mBtnEnable;
 
-  //  private ValueAnimator mAnimator;
+
     @Override
     public int getLayoutId() {
         return R.layout.activity_login;
@@ -56,18 +52,6 @@ public class LoginActivity extends BaseActivity implements LoginPresenter.View{
     @Override
     public void initWidget() {
 
-     //   mRootLly.animate().translationYBy(-1).setDuration(10000).start();
-//        mAnimator = ValueAnimator.ofFloat(-1, 1);
-//        mAnimator.setDuration(20000);
-//        mAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
-//            @Override
-//            public void onAnimationUpdate(ValueAnimator animation) {
-//                float y = (float) animation.getAnimatedValue();
-//                Log.d("Y", y+"");
-//                mRootLly.setY(y);
-//            }
-//        });
-//        mAnimator.start();
         mAccountEt.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
@@ -115,8 +99,7 @@ public class LoginActivity extends BaseActivity implements LoginPresenter.View{
         mLoginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-                startActivity(intent);
+                IntentUtils.startHome(LoginActivity.this);
             }
         });
     }
