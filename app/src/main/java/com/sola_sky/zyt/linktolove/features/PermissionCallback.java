fb636d4855fd71ac59583ffb74375028e7db2c75
Li Minyi <602699671@qq.com>
2016-03-08 00:25:51 +0800

    initial commit

---
+++ app/src/main/java/com/sola_sky/zyt/linktolove/features/PermissionCallback.java
@@ -0,0 +1,9 @@
+package com.sola_sky.zyt.linktolove.features;
+
+/**
+ * Created by Li on 2016/3/8.
+ */
+public interface PermissionCallback {
+    void onSuccessPermission();
+    void onFailedPermission();
+}
