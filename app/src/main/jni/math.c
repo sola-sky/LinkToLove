23dab958f7b665a48557aa06eb6641de0804afde
Li Minyi <602699671@qq.com>
2016-03-08 23:54:37 +0800

    initial commit

---
+++ app/src/main/jni/math.c
@@ -0,0 +1,11 @@
+//
+// Created by Li on 2016/3/8.
+//
+
+#include "com_sola_sky_zyt_linktolove_myjni_Calculate.h"
+
+JNIEXPORT jint JNICALL Java_com_sola_1sky_zyt_linktolove_myjni_Calculate_add
+        (JNIEnv *, jobject, jint param1, jint param2) {
+    return param1 + param2;
+}
+
