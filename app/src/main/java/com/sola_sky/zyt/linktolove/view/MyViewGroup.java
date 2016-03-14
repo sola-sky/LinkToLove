22d1ec859ca1bfe918f24df7b7cf1e8928ceeb01
Li Minyi <602699671@qq.com>
2016-03-14 23:19:50 +0800

    initial commit

---
+++ app/src/main/java/com/sola_sky/zyt/linktolove/view/MyViewGroup.java
@@ -0,0 +1,27 @@
+package com.sola_sky.zyt.linktolove.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.ViewGroup;
+
+/**
+ * Created by Li on 2016/3/14.
+ */
+public class MyViewGroup extends ViewGroup {
+    public MyViewGroup(Context context) {
+        super(context);
+    }
+
+    public MyViewGroup(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public MyViewGroup(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+
+    }
+}
