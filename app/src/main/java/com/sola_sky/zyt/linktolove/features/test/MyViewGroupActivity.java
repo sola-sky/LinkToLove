404c8a195a9d4cd807a42c37dd4a3a47f7fdb85a
Li Minyi <602699671@qq.com>
2016-03-18 22:45:02 +0800

    initial commit

---
+++ app/src/main/java/com/sola_sky/zyt/linktolove/features/test/MyViewGroupActivity.java
@@ -0,0 +1,34 @@
+package com.sola_sky.zyt.linktolove.features.test;
+
+import com.sola_sky.zyt.linktolove.R;
+import com.sola_sky.zyt.linktolove.app.BaseActivity;
+
+/**
+ * Created by Li on 2016/3/17.
+ */
+public class MyViewGroupActivity extends BaseActivity{
+    @Override
+    public int getLayoutId() {
+        return R.layout.activity_my_group_view;
+    }
+
+    @Override
+    public void initMember() {
+
+    }
+
+    @Override
+    public void findWidget() {
+
+    }
+
+    @Override
+    public void initWidget() {
+
+    }
+
+    @Override
+    public void initOther() {
+
+    }
+}
