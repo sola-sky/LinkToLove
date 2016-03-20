5e18e7580f260bb476b9765c2ebc0a1ebf239fcb
Li Minyi <602699671@qq.com>
2016-03-20 23:01:41 +0800

    initial commit

---
+++ app/src/main/java/com/sola_sky/zyt/linktolove/view/ViewGroupForDragHelper.java
@@ -0,0 +1,28 @@
+package com.sola_sky.zyt.linktolove.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.ViewGroup;
+
+/**
+ * Created by Li on 2016/3/20.
+ */
+public class ViewGroupForDragHelper extends ViewGroup{
+
+    public ViewGroupForDragHelper(Context context) {
+        super(context);
+    }
+
+    public ViewGroupForDragHelper(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public ViewGroupForDragHelper(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+
+    }
+}
