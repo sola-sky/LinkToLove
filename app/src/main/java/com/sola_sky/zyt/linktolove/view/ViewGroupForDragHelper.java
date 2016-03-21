3bdde4dcf337018d70ce1e7d3a1c565cae07711b
Li Minyi <602699671@qq.com>
2016-03-21 23:50:48 +0800

    practice onMeasure and onLayout methods

--- app/src/main/java/com/sola_sky/zyt/linktolove/view/ViewGroupForDragHelper.java
+++ app/src/main/java/com/sola_sky/zyt/linktolove/view/ViewGroupForDragHelper.java
@@ -2,6 +2,7 @@ package com.sola_sky.zyt.linktolove.view;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.view.View;
 import android.view.ViewGroup;
 
 /**
@@ -22,7 +23,69 @@ public class ViewGroupForDragHelper extends ViewGroup{
     }
 
     @Override
-    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        int parentDesireWidth = 0;
+        int parentDesireHeight = 0;
+        int childCount = getChildCount();
+        for(int i = 0; i < childCount; i++) {
+            View view = getChildAt(i);
+            MyMarginLayoutParams lp = (MyMarginLayoutParams) view.getLayoutParams();
+            measureChildWithMargins(view, widthMeasureSpec, 0, heightMeasureSpec, 0);
+            parentDesireWidth += view.getMeasuredWidth() + lp.leftMargin + lp.rightMargin;
+            parentDesireWidth = Math.max(parentDesireWidth, getSuggestedMinimumWidth());
+            parentDesireHeight = Math.max(parentDesireHeight, view.getMeasuredHeight() + lp.topMargin
+                    + lp.rightMargin);
+            parentDesireHeight = Math.max(parentDesireHeight, getSuggestedMinimumHeight());
+        }
+        setMeasuredDimension(resolveSizeAndState(parentDesireWidth, widthMeasureSpec, 0),
+                resolveSizeAndState(parentDesireHeight, heightMeasureSpec, 0));
+    }
 
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+        int childCount = getChildCount();
+        int offsetWidth = getPaddingLeft();
+        for (int i = 0; i < childCount; i++) {
+            View childView = getChildAt(i);
+            MyMarginLayoutParams lp = (MyMarginLayoutParams) childView.getLayoutParams();
+            childView.layout(offsetWidth + lp.leftMargin, getPaddingTop() + lp.topMargin,
+                    offsetWidth + lp.leftMargin + childView.getMeasuredWidth(), getPaddingTop()
+            + lp.topMargin + childView.getMeasuredHeight());
+            offsetWidth += lp.leftMargin + childView.getMeasuredWidth() + lp.rightMargin;
+        }
+    }
+
+    @Override
+    protected LayoutParams generateDefaultLayoutParams() {
+        return new MyMarginLayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT);
+    }
+
+    @Override
+    protected LayoutParams generateLayoutParams(LayoutParams p) {
+        return new MyMarginLayoutParams(p);
+    }
+
+    @Override
+    public LayoutParams generateLayoutParams(AttributeSet attrs) {
+        return new MyMarginLayoutParams(getContext(), attrs);
+    }
+
+    class MyMarginLayoutParams extends MarginLayoutParams {
+
+        public MyMarginLayoutParams(int width, int height) {
+            super(width, height);
+        }
+
+        public MyMarginLayoutParams(Context c, AttributeSet attrs) {
+            super(c, attrs);
+        }
+
+        public MyMarginLayoutParams(MarginLayoutParams source) {
+            super(source);
+        }
+
+        public MyMarginLayoutParams(LayoutParams source) {
+            super(source);
+        }
     }
 }
