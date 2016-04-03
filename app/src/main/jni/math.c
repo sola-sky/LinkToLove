6e43d5603f7c5480cd3b1825da30818d12795a1d
Li Minyi <602699671@qq.com>
2016-04-03 23:57:36 +0800

    test jni

--- app/src/main/jni/math.c
+++ app/src/main/jni/math.c
@@ -5,7 +5,7 @@
 #include "com_sola_sky_zyt_linktolove_myjni_Calculate.h"
 
 JNIEXPORT jint JNICALL Java_com_sola_1sky_zyt_linktolove_myjni_Calculate_add
-        (JNIEnv *, jobject, jint param1, jint param2) {
+        (JNIEnv *env, jobject obj, jint param1, jint param2) {
     return param1 + param2;
 }
 
