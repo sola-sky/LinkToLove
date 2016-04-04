fcc04ebfed33de40849e1f61b61c9ec511aef691
Li Minyi <602699671@qq.com>
2016-04-04 21:26:17 +0800

    add intents

--- app/src/main/java/com/sola_sky/zyt/linktolove/utils/IntentUtils.java
+++ app/src/main/java/com/sola_sky/zyt/linktolove/utils/IntentUtils.java
@@ -5,6 +5,9 @@ import android.content.Intent;
 
 import com.sola_sky.zyt.linktolove.features.home.HomeActivity;
 import com.sola_sky.zyt.linktolove.features.home.MainActivity;
+import com.sola_sky.zyt.linktolove.features.test.DragActivity;
+import com.sola_sky.zyt.linktolove.features.test.MyDragActivity;
+import com.sola_sky.zyt.linktolove.features.test.MyViewGroupActivity;
 import com.sola_sky.zyt.linktolove.features.userinfo.UserInfoActivity;
 
 /**
@@ -28,6 +31,20 @@ public class IntentUtils {
 
     public static void startSetting(Context context) {
         Intent intent = new Intent(context, MainActivity.class);
+        context.startActivity(intent);
+    }
+
+    public static void startMyGroupView(Context context) {
+        Intent intent = new Intent(context, MyViewGroupActivity.class);
+        context.startActivity(intent);
+    }
+    public static void startMyDragView(Context context) {
+        Intent intent = new Intent(context, DragActivity.class);
+        context.startActivity(intent);
+    }
+
+    public static void startDragView(Context context) {
+        Intent intent = new Intent(context, MyDragActivity.class);
         context.startActivity(intent);
     }
 }
