548a3a5e33ac180ac6d0aae8561fdc887856ed6f
Li Minyi <602699671@qq.com>
2016-04-06 23:13:00 +0800

    new custom view

---
+++ app/src/main/java/com/sola_sky/zyt/linktolove/view/DragViewGroup.java
@@ -0,0 +1,27 @@
+package com.sola_sky.zyt.linktolove.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.ViewGroup;
+
+/**
+ * Created by Li on 2016/4/6.
+ */
+public class DragViewGroup extends ViewGroup {
+    public DragViewGroup(Context context) {
+        super(context);
+    }
+
+    public DragViewGroup(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public DragViewGroup(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+
+    }
+}
