0c67ded731d8066beb3a8fed826604ef718f8db0
Li Minyi <602699671@qq.com>
2016-04-07 23:58:02 +0800

    add ViewDragHelper

--- app/src/main/java/com/sola_sky/zyt/linktolove/view/DragViewGroup.java
+++ app/src/main/java/com/sola_sky/zyt/linktolove/view/DragViewGroup.java
@@ -1,6 +1,7 @@
 package com.sola_sky.zyt.linktolove.view;
 
 import android.content.Context;
+import android.support.v4.widget.ViewDragHelper;
 import android.util.AttributeSet;
 import android.view.ViewGroup;
 
@@ -8,6 +9,8 @@ import android.view.ViewGroup;
  * Created by Li on 2016/4/6.
  */
 public class DragViewGroup extends ViewGroup {
+
+    private ViewDragHelper mDragHelper;
     public DragViewGroup(Context context) {
         super(context);
     }
@@ -23,5 +26,15 @@ public class DragViewGroup extends ViewGroup {
     @Override
     protected void onLayout(boolean changed, int l, int t, int r, int b) {
 
+    }
+
+    @Override
+    protected void onFinishInflate() {
+        super.onFinishInflate();
+        if (getChildCount() == 2) {
+
+        } else {
+            throw new
+        }
     }
 }
