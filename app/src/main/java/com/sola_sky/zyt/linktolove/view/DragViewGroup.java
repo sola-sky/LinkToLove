0b573f047e63ca28fb6277707c2d35e38b33e9bc
Li Minyi <602699671@qq.com>
2016-04-09 22:52:56 +0800

    practice onMeasure and other methods

--- app/src/main/java/com/sola_sky/zyt/linktolove/view/DragViewGroup.java
+++ app/src/main/java/com/sola_sky/zyt/linktolove/view/DragViewGroup.java
@@ -3,6 +3,7 @@ package com.sola_sky.zyt.linktolove.view;
 import android.content.Context;
 import android.support.v4.widget.ViewDragHelper;
 import android.util.AttributeSet;
+import android.view.View;
 import android.view.ViewGroup;
 
 /**
@@ -23,6 +24,31 @@ public class DragViewGroup extends ViewGroup {
         super(context, attrs, defStyleAttr);
     }
 
+    public void init() {
+        mDragHelper = ViewDragHelper.create(this, 1.0f, new MyDragCallback());
+        mDragHelper.setEdgeTrackingEnabled(ViewDragHelper.EDGE_LEFT);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        int parentDesireWidth = 0;
+        int parentDesireHeight = 0;
+        int childCount = getChildCount();
+        for (int i = 0; i < childCount; i++) {
+            View view = getChildAt(i);
+            MyMarginLayoutParams lp = (MyMarginLayoutParams) view.getLayoutParams();
+            measureChildWithMargins(view, widthMeasureSpec, 0, heightMeasureSpec, 0);
+
+            parentDesireWidth = Math.max(parentDesireWidth, view.getMeasuredWidth() + lp.leftMargin
+            + lp.rightMargin);
+            parentDesireHeight += view.getMeasuredHeight() + lp.topMargin + lp.bottomMargin;
+        }
+        parentDesireWidth = Math.max(parentDesireWidth, getSuggestedMinimumWidth());
+        parentDesireHeight = Math.max(parentDesireHeight, getSuggestedMinimumHeight());
+        setMeasuredDimension(resolveSizeAndState(parentDesireWidth, widthMeasureSpec, 0),
+                resolveSizeAndState(parentDesireHeight, heightMeasureSpec, 0));
+    }
+
     @Override
     protected void onLayout(boolean changed, int l, int t, int r, int b) {
 
@@ -35,6 +61,46 @@ public class DragViewGroup extends ViewGroup {
 
         } else {
             throw new IllegalArgumentException("view count must be 2");
+        }
+    }
+
+    @Override
+    protected LayoutParams generateDefaultLayoutParams() {
+        return new MyMarginLayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT);
+    }
+
+    @Override
+    protected LayoutParams generateLayoutParams(LayoutParams p) {
+        return new MyMarginLayoutParams(p);
+    }
+
+    @Override
+    public LayoutParams generateLayoutParams(AttributeSet attrs) {
+        return new MyMarginLayoutParams(getContext(), attrs);
+    }
+
+    class MyMarginLayoutParams extends MarginLayoutParams {
+        public MyMarginLayoutParams(Context c, AttributeSet attrs) {
+            super(c, attrs);
+        }
+
+        public MyMarginLayoutParams(int width, int height) {
+            super(width, height);
+        }
+
+        public MyMarginLayoutParams(MarginLayoutParams source) {
+            super(source);
+        }
+
+        public MyMarginLayoutParams(LayoutParams source) {
+            super(source);
+        }
+    }
+
+    class MyDragCallback extends ViewDragHelper.Callback {
+        @Override
+        public boolean tryCaptureView(View child, int pointerId) {
+            return false;
         }
     }
 }
