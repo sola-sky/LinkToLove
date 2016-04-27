26baf08f73dc461e8deae91fcf0b91412bf53b4f
Li Minyi <602699671@qq.com>
2016-04-27 08:00:24 +0800

    use case

---
+++ app/src/main/java/com/sola_sky/zyt/linktolove/google/usecase/UseCase.java
@@ -0,0 +1,45 @@
+package com.sola_sky.zyt.linktolove.google.usecase;
+
+/**
+ * Created by Li on 2016/4/27.
+ */
+public abstract class UseCase<Q extends UseCase.RequestValues,
+        V extends UseCase.ResponseValues> {
+
+    public Q mRequestValues;
+    public UseCaseCallback<V> mUseCaseCallback;
+
+    public Q getRequestValues() {
+        return mRequestValues;
+    }
+
+    public void setRequestValues(Q requestValues) {
+        this.mRequestValues = requestValues;
+    }
+
+    public UseCaseCallback<V> getUseCaseCallback() {
+        return mUseCaseCallback;
+    }
+
+    public void setUseCaseCallback(UseCaseCallback<V> useCaseCallback) {
+        this.mUseCaseCallback = useCaseCallback;
+    }
+
+    void run() {
+        executeUseCase(mRequestValues);
+    }
+    protected abstract void executeUseCase(Q requestValues);
+
+    public interface RequestValues {
+
+    }
+
+    public interface ResponseValues {
+
+    }
+
+    public interface UseCaseCallback<R> {
+        void onSuccess(R response);
+        void onError(Error error);
+    }
+}
